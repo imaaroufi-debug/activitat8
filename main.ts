fc10cbbf7cbc56abb2675db663a10f879ef5c343
imaaroufi-debug <imaaroufi@iesperefontdevila.cat>
2026-03-03 13:28:12 +0100

    Fitxers inicials per al projecte MakeCode

---
+++ main.ts
@@ -0,0 +1,9 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Sad)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showString("plaaaaaaaaaaaa")
+})
+basic.forever(function () {
+	
+})
