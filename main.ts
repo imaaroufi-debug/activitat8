edc2aeff909d24483bde87a37c1089b5bd5ff65a
imaaroufi-debug <imaaroufi@iesperefontdevila.cat>
2026-03-03 13:29:47 +0100

    Actualització main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,12 @@
 input.onButtonPressed(Button.A, function () {
     basic.showIcon(IconNames.Sad)
 })
+input.onButtonPressed(Button.AB, function () {
+    for (let index = 0; index < 4; index++) {
+        basic.showIcon(IconNames.Heart)
+        basic.showIcon(IconNames.Sad)
+    }
+})
 input.onButtonPressed(Button.B, function () {
     basic.showString("plaaaaaaaaaaaa")
 })
